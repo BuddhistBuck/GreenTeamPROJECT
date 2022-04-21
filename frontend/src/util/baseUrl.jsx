e1c37c90813fb8a5aea60b489e64af75e566ecac
chris-p <59159608+cchris-p@users.noreply.github.com>
2022-04-21 13:52:48 -0400

    Build for netlify

--- frontend/src/util/baseUrl.jsx
+++ frontend/src/util/baseUrl.jsx
@@ -1,2 +1,2 @@
-// export const baseUrl = "https://court-reporter-pro.herokuapp.com"
-export const baseUrl = "http://localhost:4000";
+export const baseUrl = "https://court-reporter-pro.herokuapp.com"
+// export const baseUrl = "http://localhost:4000";
